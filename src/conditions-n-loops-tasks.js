9165dbe2e5f05b910ee057f4dcace1620b1d1f15
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2024-12-16 21:29:02 +0300

    feat: add getIndexOf

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -162,8 +162,12 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  for (let i = 0; i < str.length; ) {
+    if (str[i] === letter) return i;
+    i += 1;
+  }
+  return -1;
 }
 
 /**
