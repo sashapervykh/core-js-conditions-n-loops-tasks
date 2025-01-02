8233954cc790d725ba973879b6f6d4b10ee3046a
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-01-02 15:55:59 +0300

    feat: canQueenCaptureKing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -63,8 +63,12 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x) return true;
+  if (queen.y === king.y) return true;
+  if (queen.x + queen.y === king.x + king.y) return true;
+  if (queen.x - king.x === queen.y - king.y) return true;
+  return false;
 }
 
 /**
